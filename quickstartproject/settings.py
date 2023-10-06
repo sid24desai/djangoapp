b2af26cf1f709ad3641262bfd6b47d44df7f90e5
sid24desai <sid24desai@gmail.com>
2023-10-06 11:47:25 +0530

    first commit

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = '1234567890'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://valleywebapp32.azurewebsites.net']
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
